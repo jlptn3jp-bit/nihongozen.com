a265c9ae0579a195358ddf766c0e51ea6ddf6a97
jlptn3jp-bit <jlptn3.jp@gmail.com>
2026-04-30 18:26:06 +0530

    Update kanji.js

--- js/kanji.js
+++ js/kanji.js
@@ -1,20 +1,18 @@
-let kanjiList = [];
-let kIndex = 0;
+async function loadKanji() {
+    const res = await fetch("data/kanji.json");
+    const data = await res.json();
 
-async function loadKanji(){
-  let res = await fetch("data/kanji.json");
-  kanjiList = await res.json();
-  kIndex = 0;
-  showKanji();
-}
+    let output = "";
 
-function showKanji(){
-  document.getElementById("kdata").innerText =
-    kanjiList[kIndex].kanji + " = " + kanjiList[kIndex].meaning;
-}
+    data.forEach(k => {
+        output += `
+            <div class="kanji-card">
+                <h2>${k.character}</h2>
+                <p>Meaning: ${k.meaning}</p>
+                <p>Reading: ${k.reading}</p>
+            </div>
+        `;
+    });
 
-function nextKanji(){
-  kIndex++;
-  if(kIndex >= kanjiList.length) kIndex = 0;
-  showKanji();
+    document.getElementById("kanji").innerHTML = output;
 }
