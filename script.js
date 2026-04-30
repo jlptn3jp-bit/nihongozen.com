192d399322434a45a77a2fc83f05643bcbbceb38
jlptn3jp-bit <jlptn3.jp@gmail.com>
2026-04-30 13:56:07 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function login(){
-let name=document.getElementById("user").value;
-if(name==="")return;
-
-localStorage.setItem("user",name);
-localStorage.setItem("progress",0);
-
-window.location.href="dashboard.html";
-}
