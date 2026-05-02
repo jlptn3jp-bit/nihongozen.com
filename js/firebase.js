8663d0de5e945a60ebb74ab1814496df9f5dadd0
jlptn3jp-bit <jlptn3.jp@gmail.com>
2026-05-02 17:07:07 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,2 +1,12 @@
-export const auth = {}; // placeholder
-export const db = {};
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
