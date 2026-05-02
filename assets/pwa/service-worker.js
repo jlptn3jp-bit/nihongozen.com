549b1b1faaa89405c394a77e5c18557a928f945f
jlptn3jp-bit <jlptn3.jp@gmail.com>
2026-05-02 15:40:45 +0530

    Update service-worker.js

--- assets/pwa/service-worker.js
+++ assets/pwa/service-worker.js
@@ -5,7 +5,6 @@ self.addEventListener('activate', event => {
   clients.claim();
 });
 self.addEventListener('fetch', event => {
-  // Basic cache strategy
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
