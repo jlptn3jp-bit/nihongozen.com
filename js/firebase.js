c89e8858d0edf0c27962c1609036d006dee522a2
Nihongo_zen <jlptn3.jp@gmail.com>
2026-05-04 01:23:11 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,10 @@
-// js/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_ID",
   appId: "YOUR_APP_ID"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const app = initializeApp(firebaseConfig);
