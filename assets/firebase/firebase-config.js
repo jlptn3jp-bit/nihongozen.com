7ddc0c3f28f3308ab56c554449a275ac04aa155a
Nihongo_zen <jlptn3.jp@gmail.com>
2026-05-03 11:30:56 +0530

    Update firebase-config.js

--- assets/firebase/firebase-config.js
+++ assets/firebase/firebase-config.js
@@ -1,10 +1,13 @@
-// Initialize Firebase
+// Firebase v8 style (simple use)
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_PROJECT",
   projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
+  storageBucket: "YOUR_PROJECT.appspot.com",
   messagingSenderId: "YOUR_SENDER_ID",
   appId: "YOUR_APP_ID",
 };
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
