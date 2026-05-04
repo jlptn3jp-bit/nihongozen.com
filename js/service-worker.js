6580bca0880bdcaf9ab40ba91cf1b687d7f357dc
Nihongo_zen <jlptn3.jp@gmail.com>
2026-05-04 14:12:46 +0530

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,7 +1,35 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker Installed");
+const CACHE_NAME = 'nihongogen-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
